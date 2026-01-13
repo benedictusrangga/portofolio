1f045e7cc7e52e7abebf159e3114289840d919d2
benedictusrangga <benedictus.rangga9@gmail.com>
2026-01-14 00:47:51 +0700

    Update with correct Supabase anon key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://foojrcvrjtvjkcmblmvh.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvb2pyY3ZyanR2amtjbWJsbXZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY4MjU0NzQsImV4cCI6MjA1MjQwMTQ3NH0.sb_publishable_vlSDd9Pz-PkIPIUc4xBTbw_TIyX8BS_';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvb2pyY3ZyanR2amtjbWJsbXZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgzMjQ2NTYsImV4cCI6MjA4MzkwMDY1Nn0.zqAOEbhGbep9254TDo6DhnnVqGyUzNsYfwF2hfw43BA';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
